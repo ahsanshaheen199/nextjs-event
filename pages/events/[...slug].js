30afa356fcef67f40ea55a8dbed254587ef1fadf
Ahsan Habib Shaheen <ahsanshaheen199@gmail.com>
2021-12-25 13:33:07 +0600

    Added featured event page

---
+++ pages/events/[...slug].js
@@ -0,0 +1,7 @@
+export default function FilteredEventpage() {
+    return (
+        <div>
+            <h1>Filtered Event Page</h1>
+        </div>
+    )
+}
---
+++ pages/events/index.js
@@ -0,0 +1,7 @@
+export default function EventListpage() {
+    return (
+        <div>
+            <h1>All Event Page</h1>
+        </div>
+    )
+}
